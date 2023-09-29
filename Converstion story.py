426e6a724fc4fac1eeb5c4883cbd960924ea0751
LGegino <145944233+LGegino@users.noreply.github.com>
2023-09-29 08:19:21 +0800

    Add files via upload

---
+++ Converstion story.py
@@ -0,0 +1,167 @@
+print("__________________________________________________________")
+
+pounds = float(input('Enter weight in Pounds(Lbs) to Convert into Kilograms:'))
+kilo_grams = pounds * 0.453592
+print(pounds,' Pounds (Lbs) are equal to', kilo_grams,'Kilograms (Kgs)')
+print("_ _ _ _ _ _ _ _ _")
+print("Rounded result:")
+print(round(kilo_grams,1)) 
+
+print("__________________________________________________________")
+
+miles = float(input('Enter distance in Miles (mi) to Convert into Kilometers:'))
+kilometers = miles * 1.60934
+print(miles,' Miles (mi) equals to', kilometers,'Kilogmeters (km)')
+print("_ _ _ _ _ _ _ _ _")
+print("Rounded result:")
+print(round(kilometers,1))
+
+print("__________________________________________________________")
+
+fahrenheit = float(input('Enter temprature in Fahrenheit(F) to Convert into Celcius:'))
+celcius = (fahrenheit - 32) * 5/9
+print(fahrenheit,' Fahrenheit (F) equates to', celcius,'Celcius (C)')
+print("_ _ _ _ _ _ _ _ _")
+print("Rounded result:")
+print(round(celcius,1))
+
+print("__________________________________________________________")
+
+print("======================================================")
+print("Students in a class:")
+print("======================================================")
+print("Marci 18")
+print("Maneo 19")
+print("Mento 18")
+print("Melina 17")
+print("Mei Mei 17")
+print("Mang Juan 20")
+print("Mancy 16")
+print("Marmadook 18")
+print("Mark 18")
+print("Marly 19")
+print("======================================================")
+inp_lst = [18, 19, 18, 17, 17, 20, 16, 18, 18, 19]
+
+sum_lst = sum(inp_lst)
+
+lst_avg = sum_lst/len(inp_lst)
+print("Average age on the list:\n") 
+print(lst_avg) 
+
+print("__________________________________________________________")
+
+name1 = "Abel"
+name2 = "Cain"
+name3 = "Leena"
+name4 = "Vista"
+name5 = "Tempestus"
+
+Magic1 = "sea dragon"
+Magic2 = "WATER BODY"
+Magic3 = "lightning"
+
+item1 = "axe"
+item2 = "staff"
+item3 = "bow"
+item4 = "arrow"
+item5 = "sword"
+item6 = "spear"
+item7 = "shield"
+item8 = "mace"
+item9 = "canon"
+item10 = "club"
+item11 = "mechanical bird"
+item12 = "forbidden fruit"
+item13 = "armor"
+
+print("=================")
+print("Red Tide")
+print("By : Leigh Egino")
+print("=================")
+
+print(f"The leviathan fell, and the party was victorious- but another foe seems to have revealed himself from the ruins.")
+
+print(f"[At long last we meet brother!], shouted a man covered in red velvet {item13} rocking a glorious dark beard- drenched and dripping with blood.")
+print(f"He walked forward to greet this person whom he called brother with heavy steps- pounding the ground.") 
+print(f"Dust flew from the soles of his boots, and the ground beneath him cracked with each step. Blood continued to drip on the soil and concrete, leaving a long trail of dark red stains on the ground.")
+
+print(f"The person whom this red warrior claimed to be his brother, is a young handsome man who wore shattered {item13}- having long red hair.")
+print(f"There was a caving hole in his breastplate, piercing his torso. Boots, so worn out, they could fall apart within a few steps.") 
+
+print(f"The broken warrior felt intimidated by this moving beast of a man. [Are you really my brother?] he said.") 
+print(f"The red coated man while approaching him says [have you forgotten? We escaped paradise a long time ago, the moment Father casted you out for eating the so-called {item12}].")
+print(f" [I don't remember any of that], exclaimed the man in broken {item13}.")
+print("The blood drenched warrior continued, [Have you forgotten how you were TRICKED! by the beast with no arms nor legs?].")
+print("[Don't play a fool brother],")
+print("I know you would never forget that day- the day you fell into despair and rage making leading you to a blind frenzy and ripping off the wings of the angel who stood guard near paradise's entrance]")
+
+print(f"Silence took place for a couple of minutes, then the broken man spoke- [I remember…], he said quietly.") 
+print(f"The broken man then let out a burst of sinister laughs, [you've seem to have grown older dear brother]- he said jokingly.") 
+
+print(f"[Finally! It has been too long, {name1}] the bloodied man joyfully said.") 
+print(f"The shattered brother responded by snapping his fingers, and immediately- an {item4} bolted past him, piercing through the {item13} of the crimson drenched man- {name1}.")
+
+print(f"[I couldn't expect less from an underhanded killer… {name2}!] {name1} called out the shattered man's name with a shout.") 
+
+print(f"[I have risen from my own pool of blood, do you really think cheap tricks such as a toothpick flung by a string would harm me? I've dealt with way worse, SO DON'T DISRESPECT ME WITH THAT PUNY GREETING] {name1} roared.") 
+print(f"{name1} charged in the direction of {name2}, pulling out the {item1} that slew countless beings.") 
+
+print(f"A loud crack of thunder fell upon {name1} and covered the grounds with dust and flying debris, seemingly interrupting his advance.") 
+print(f"But that wasn't enough to stop {name1}s ground shattering charge, he gripped his {item1} inching closer to {name2}-") 
+
+print(f"but he was disrupted again by a {Magic1} formed from water as it charged towards {name1} pushing him back with a powerful current of never ending water.") 
+print(f"Despite the strong force opposing {name1}, he didn't back down and willfully went against the roaring tide brought upon him.") 
+print(f"The tide of water seemed to never cease and stepping to the side would make {name1} lose his footing.") 
+print(f"Suddenly, endless {item4}s flowed through the stream which pierced {name1} more and more- turning the the focused stream of water into a reddish liquid. This didn't stop {name1}, despite him drowning slowly and bleeding excessively from all the damage he's sustaining- he still defies the violent concentrated pump of water. Gradually, the water pushing {name1} turned red infusing with his blood.")
+
+print(f"{name2}, feeling cocky at the sorry state his brother is in, let out a laugh- [is this all you can do brother? You haven't changed, you still choose to stay stubborn and relentlessly charge into danger when there is always an easier path]. {name2} made a signal, and yellow light glowed behind him- moments later {Magic3} struck the murky water and electrocuted the wave violently and visibly- causing ripples and splashes all throughout. [It's over brother, all you have done to find me was in vain]- {name2} says with an over confident tone.")
+
+print(f"But something was wrong, the water that flowed towards {name1}- became clearer and clearer, with no one inside it. Soon, the strong flow of water halted. {name2} shouted [{name3} why did  you stop!?]. Then a scream was heard, [{name3}!!] {name2} shouted.") 
+print(f"Out of the shadows, {name1} emerged with {item4}s stuck in and all over his body. A woman gripped by his left hand, and an {item1} on the right- aiming for the throat.") 
+
+print(f"{name2} wasted no time and snapped his fingers, {item4}s hailed from the sky.") 
+print(f"[USE {Magic2} {name3}!] {name2} shouted in a panic. But before {name3} could mutter a few words, {name1} threw her at the barrage of {item4}s and took a strong dash away from it. An {item4} pierced {name1}'s calf, which made him trip and fall with a loud thump. However, he wasn't that injured- in comparison to {name3} who fell after the throw, being shot by multiple {item4}s. The {item4}s in {name3} twisted and dug deeper as she fell onto the ground.")
+
+print(f"{name2}, in a panic, rushed towards {name3}. {name1}, seeing this, pulled out the {item4} from his calf and threw it like a dart- going in and exiting {name1}s thigh, which made {name2} take a violent fall and slide across the rough floor. But despite this, {name2} launches himself forward with his bloody leg as {name1} tries to intercept.") 
+print(f"{Magic3} crackled and struck {name1}. The {item4} that pierced {name1}'s calf seemed to have slowed him down. This was followed by more {item4}s flying towards him. {name1}, stunned by the obscene amount of electricity, was easily pelted by the sharp flying objects- dropping him to his knees and falling onto his side resembling a dead porcupine.")
+
+print(f"{name2} finally beside {name3}, shed a tear after a closer look at her pathetic state. She continues to bleed- drenching the soil beneath her and giving it a darker pigment. {name2} hastily lifts her head onto his lap.") 
+print(f"Then {name3} let out her final words in a faint voice,[Please make that paradise happen, for the sake of the unfortunate ones like us]. {name3} dies, and {name2} closes her eyes. He hugged her dead corpse and weeped.")
+
+print(f"Then another voice was heard. [Where were you when I was bleeding from my skull? Were you there to show as much love to me as you did to her in her dying moments? Were you there to even mourn me for at least a mere second when I was lifeless and betrayed!?]- {name1} says, as he gets up from a pool of blood beneath him.") 
+
+print(f"[How are you not dead yet brother?] {name2} says, with a clearly annoyed face.") 
+print(f"{name1} replied, [I should ask the same with you? How is it, that Dear father hasn't smithen you from the heavens after what you have done to his servant?]. {name1} chuckled and a red symbol glowed from his forehead. {name1} said in a shaky yet joyous tone [Because, our father has cursed me to roam this land… Forever].")
+
+print(f"[You're lying and I know it], replied {name1}.") 
+print(f"[Why would I lie? I have proof right here], says {name2} while pointing at his forehead.") 
+print(f"[{name2}… where are the wings of the angel you tore off? That symbol and curse never belonged to you, you merely stole it].") 
+print(f"*The bloody {item13} of {name1} reacts and shrieks*,") 
+print(f"[Meet the servant you murdered] {name1} says, while patting the armor's chestpiece.")
+print(f"{name2} after hearing this froze and smiled, then he spoke- [it's no use lying, I guess], then he ripped off his {item13}. Wings sprouted from his back, [Tada!] {name2} says- in a joyful tone.") 
+
+print(f"{name1} in shock shouted [YOUR AN ABOMINATION, A DISGRACE TO ALL CREATION, YOU NEPHILIM!].") 
+print(f"[Now explain to me MORE brother, how are you alive? After all these years and being left in such an unbearable state?], {name2} asked.") 
+print(f"{name1} replied, [Father always favored my offerings, and when my life ended by your hands back at the gates of paradise- Father changed what he wanted of my offerings. He told me to offer him what you used to offer {name2}… the dead].") 
+print(f"{name2} was speechless,") 
+print(f"but {name1} continued- [From my harvest of greens to a harvest of red, I now do not reap the plants that I have sown- but reap the blood of our fathers enemies. This {item13} is made from the blood of the angel you slew on the gates of paradise, given to me by father- and now, it has come back to take back what was once his].") 
+print(f"Able charged, but this time not at {name1}- but at the two hidden people who have been striking him from the shadows. Out of nowhere, {name2} came crashing onto {name1} and pinning him to the ground, [{name4} , {name5} RUN!] {name2} shouted. But following his words was a hand that grabbed {name2} by the face and slammed him onto the ground- {name1} was stunned. [I was always the stronger one, you sneaky little bastard!], {name1} shouted. He proceeded to recover and stand up, then kicking {name2} on the ribs- flinging him back a few meters. [Now, for the heretics who chose to follow this beast], {name1} says to himself as he sprints and leaves a trail of boiling hot blood.") 
+
+print(f"Moments later, {name4} and {name5} who are running, spot {name2} in pursuit of them- advancing with inhuman speed. {name4} halted and pulled out a {item3}. [WHAT ARE YOU DOING? KEEP GOING!] {name5} shouted. [NO! I will hold him back while you run and call in reinforcements], says {name4}. {name5} felt conflicted but ran ahead and launched a {item11} out of his pocket. He then used his long {item2} to create streaks of lighting to erect a wall of stunning shock power. {name5} then turned back to see his friend {name4} cut down by {name1}'s blood thirsty {item1}. A shriek of pain was heard as {name5} ran straight forward- never looking back.")
+
+print(f"{name1}, upon turning around in the direction of {name2}- was met with a {item6} hurled at him. {name1} barely dodged it- being grazed by the neck. {name2} then fluttered down with his wings, which had turned soaking red, as if it were bleeding. [Seems like the angel of war wants his wings back] {name2} says- with a big smile. The {item13} of {name1} reacts to the bleeding wings and spikes protrude from all over the {item13}. {name1} in response summoned a shield and recalled his {item6}. {name1} lunges forward, swinging his {item1} as blood ejects from the blade and howls as it cuts through the wind. {name2} blocks this with his shield and stabs {name1} with his {item6} through the {item13} and stomach.")
+
+print(f"{name1} roared and despite receiving a fatal blow, continued to move forward and dig the {item6} deeper to grab {name2}'s shield. {name1} took a swing at {name2} and cut his chest open. {name2}'s blood spurted out onto {name1}'s eyes, and {name1} kicked him back with a force of 10 Rhinos charging at him. Hastily, {name1} wiped off the blood from his eyes and upon regaining his vision, {name2} was gone. [COWARD WHERE ARE YOU?] shouted {name1}.")
+print(f"But instead of his brother, something else responded with a loud sounding of a horn and thumping boots of a thousand men.")
+print(f"Offensives, such as {item5}s, sharp shields, {item6}s, {item3}s, {item8}s, {item10}s, {item9}s, sorcery, and dragons all aimed at {name1}'s direction- with {name2} hovering over them all. {name2} signaled for a charge, and {name1} let out a war cry, shaking the land. Both men and beast rushed towards {name1} and the warrior drenched in blood responded with a furious shout of words, [THIS WON'T STOP ME, RUN, HIDE, BEG MERCY FROM OUR FATHER- NOTHING WILL SAVE YOU, NOT EVEN YOUR ARMY. I HAVE MAIMED COUNTLESS HERETICS AND DEMONS, THIS ISN'T THE END OF ME. I WILL DRENCH MY {item13} AND {item1} WITH THE BLOOD OF YOUR MEN, THEN KILL YOU WITH IT!]")
+
+print(f"{name1} leaped into the wave of soldiers and blood splattered everywhere, {name1} cut and gored all who came in contact with him. {name1} was shanked, cut, burned, pierced and bludgeoned- but the vengeful warrior continued to move and bathe in the blood of {name2}'s men. {name1}s limbs were torn, disintegrated, broken, and damaged beyond repair. But the fury and rage of {name1} and the {item13} relentlessly restored {name1} gradually to peak form time and time again. {name1} wrestled dragons from the sky leaping onto them and cutting their heads off, wounding them and crashing down with it. This was a massacre for {name1}, and {name2}'s army unrelentlessly kept pouring onto the savage {name1}. {name1} opened his mouth and said [Father gave me a title upon resurrection, I am- The red tide, use it].")
+
+print("====================")
+
+input('Press ENTER to exit')
+
+print("====================")
+
+
